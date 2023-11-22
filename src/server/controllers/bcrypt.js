09d54f3af12da8e16467f5bcd52ca4db28d49377
Sung Ku Kang <sungkuka@gmail.com>
2023-11-22 11:46:29 -0800

    reorganized structure

--- src/server/controllers/bcrypt.js
+++ src/server/controllers/bcrypt.js
@@ -1,30 +1,32 @@
 const bcrypt = require('bcrypt');
 
-// Middleware for hashing password
-const hashPassword = async (req, res, next) => {
-  try {
-    const BCRYPT_SALT_ROUNDS = 10;
-    const salt = await bcrypt.genSalt(BCRYPT_SALT_ROUNDS);
-    req.body.password = await bcrypt.hash(req.body.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
-};
-
-// Middleware for verifying password
-const verifyPassword = async (req, res, next) => {
-  try {
-    const { user, password } = req;
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) {
-      // Return a generic error message
-      return res.status(401).send('Invalid username or password');
+const bcryptController = {
+  // Middleware for hashing password
+  hashPassword: async (req, res, next) => {
+    try {
+      const BCRYPT_SALT_ROUNDS = 10;
+      const salt = await bcrypt.genSalt(BCRYPT_SALT_ROUNDS);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+      next();
+    } catch (error) {
+      next(error);
     }
-    next();
-  } catch (error) {
-    next(error);
-  }
+  },
+
+  // Middleware for verifying password
+  verifyPassword: async (req, res, next) => {
+    try {
+      const { user, password } = req;
+      const validPassword = await bcrypt.compare(password, user.password);
+      if (!validPassword) {
+        // Return a generic error message
+        return res.status(401).send('Invalid username or password');
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
 };
 
-module.exports = { hashPassword, verifyPassword };
+module.exports = bcryptController;
