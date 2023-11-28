24af0ba0af0494d1159533d972f125a30b4a86c5
yqtram <yvonneqtram@gmail.com>
2023-11-27 19:04:37 -0800

    completed cluster vis controller

--- src/server/controllers/viewController.js
+++ src/server/controllers/viewController.js
@@ -1,14 +1,12 @@
-// import { ModelTraining } from '@mui/icons-material';
-// import React, { useEffect, useState } from 'react';
 const k8s = require('@kubernetes/client-node');
 
 const viewController = {
+  // Middleware for buillding Kubernetes cluster data
   get: async (req, res, next) => {
     const kc = new k8s.KubeConfig();
     kc.loadFromDefault();
 
     const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-    // TESTING
     const appsV1Api = kc.makeApiClient(k8s.AppsV1Api);
 
     try {
@@ -17,37 +15,30 @@ const viewController = {
       const services = await k8sApi.listServiceForAllNamespaces();
       const deployments = await appsV1Api.listDeploymentForAllNamespaces();
 
-      const getsvcPodRelation = () => {
-        // services.spec.selector
-        // 1 {
-        //   'app.kubernetes.io/instance': 'prometheus',
-        //   'app.kubernetes.io/name': 'alertmanager'
-        // }
-        // 2 {
-        //   'app.kubernetes.io/instance': 'prometheus',
-        //   'app.kubernetes.io/name': 'alertmanager'
-        // }
-        // MATCH ABOVE WITH pods.metadata.labels
-      };
+      // Map corresponding pods and services
+      const svcPodRelation = services.body.items.reduce(
+        (accumulator, service) => {
+          const matchingPods = pods.body.items
+            .filter((pod) => {
+              const podLabels = pod.metadata.labels;
+              const svcSelector = service.spec.selector;
 
-      const mapSvcPod = (podLabels, svcSelector) => {
-        for (const key of Object.keys(svcSelector)) {
-          if (!podLabels[key] || podLabels[key] !== svcSelector[key])
-            return false;
-        }
+              if (!podLabels || !svcSelector) return false;
 
-        return true;
-      };
+              return Object.keys(svcSelector).every(
+                (key) => podLabels[key] === svcSelector[key]
+              );
+            })
+            .map((pod) => pod.metadata.name);
 
-      // gpt
-      // const mapSvcPod = (podLabels, svcSelector) => {
-      //   return Object.keys(svcSelector).every(
-      //     (key) => podLabels[key] === svcSelector[key]
-      //   );
-      // };
+          accumulator[service.metadata.name] = matchingPods;
+          return accumulator;
+        },
+        {}
+      );
 
-      ////////////// fetch k8s data //////////////
-      const LOGGER = {
+      // Fetch Kubernetes cluster data
+      res.locals.data = {
         nodes: nodes.body.items.map((node) => node.metadata.name),
         pods: pods.body.items.map((pod) => ({
           name: pod.metadata.name,
@@ -55,18 +46,13 @@ const viewController = {
         })),
         services: {
           name: services.body.items.map((service) => service.metadata.name),
-          svcPodRelation: getsvcPodRelation(),
+          svcPodRelation,
         },
         deployments: deployments.body.items.map(
           (deployment) => deployment.metadata.name
         ),
       };
 
-      pods.body.items.forEach((pod, idx) => {
-        console.log(idx, pod.metadata.labels);
-      });
-
-      res.locals.data = LOGGER;
       return next();
     } catch (error) {
       return next(error);
--- src/server/routers/viewRouter.js
+++ src/server/routers/viewRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const viewController = require('../controllers/viewController');
 
 router.get('/get', viewController.get, (req, res) => {
-  return res.status(200);
+  return res.status(200).json(res.locals.data);
 });
 
 module.exports = router;
