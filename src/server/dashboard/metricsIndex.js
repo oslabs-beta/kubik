08216ce251cab3d17887105e329018457ec235a0
Sung Ku Kang <sungkuka@gmail.com>
2023-11-30 12:49:26 -0800

    add more grafana graphs for frontend

--- src/server/dashboard/metricsIndex.js
+++ src/server/dashboard/metricsIndex.js
@@ -1,11 +1,11 @@
 const metrics = {
   nodeExporter: {
     uid: 'rYdddlPWk',
-    panelId: [14, 15],
+    panelId: [14, 15, 74, 77, 78, 84, 152, 155],
   },
   kubernetesViewsNodes: {
     uid: 'k8s_views_nodes',
-    panelId: [18, 24],
+    panelId: [3, 13, 18, 24, 48],
   },
 };
 
